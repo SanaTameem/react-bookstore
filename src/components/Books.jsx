87b6c580f3b425151c88906c78eb923e78f12556
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-07-22 22:12:23 +0430

    Update src/components/Books.jsx file

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import BookItem from './BookItem';
 import Form from './Form';
 import '../styles/Books.css';
 
 function Books() {
+  const booksData = [
+    { id: uuidv4(), title: 'Book 1', author: 'Author 1' },
+    { id: uuidv4(), title: 'Book 2', author: 'Author 2' },
+    { id: uuidv4(), title: 'Book 3', author: 'Author 3' },
+  ];
   return (
     <div className="book-list">
-      <BookItem />
-      <BookItem />
-      <BookItem />
+      {booksData.map((book) => (
+        <BookItem key={book.id} book={book} />
+      ))}
       <Form />
     </div>
   );
