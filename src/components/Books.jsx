e5890432f6866d45a73cf24ba39ec46f1c72f3e7
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-07-22 23:38:27 +0430

    Run linters

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,8 +1,31 @@
+// import React from 'react';
+// import { v4 as uuidv4 } from 'uuid';
+// import BookItem from './BookItem';
+// import Form from './Form';
+// import '../styles/Books.css';
+
+// function Books() {
+//   const booksData = [
+//     { id: uuidv4(), title: 'Book 1', author: 'Author 1' },
+//     { id: uuidv4(), title: 'Book 2', author: 'Author 2' },
+//     { id: uuidv4(), title: 'Book 3', author: 'Author 3' },
+//   ];
+//   return (
+//     <div className="book-list">
+//       {booksData.map((book) => (
+//         <BookItem key={book.id} book={book} />
+//       ))}
+//       <Form />
+//     </div>
+//   );
+// }
+
+// export default Books;
+
+// components/Books.jsx
 import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import BookItem from './BookItem';
-import Form from './Form';
-import '../styles/Books.css';
+import BookList from './BookList';
 
 function Books() {
   const booksData = [
@@ -10,12 +33,10 @@ function Books() {
     { id: uuidv4(), title: 'Book 2', author: 'Author 2' },
     { id: uuidv4(), title: 'Book 3', author: 'Author 3' },
   ];
+
   return (
-    <div className="book-list">
-      {booksData.map((book) => (
-        <BookItem key={book.id} book={book} />
-      ))}
-      <Form />
+    <div>
+      <BookList booksData={booksData} />
     </div>
   );
 }
