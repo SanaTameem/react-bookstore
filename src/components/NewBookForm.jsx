ea70a0ef065b5ba902c26c8066f6700363284299
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-08-04 23:37:18 +0430

    Update src/components/NewBookForm.jsx file

--- src/components/NewBookForm.jsx
+++ src/components/NewBookForm.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { postBook } from '../redux/books/booksSlice';
+import '../styles/NewBookForm.css';
 
 const NewBookForm = () => {
   const [title, setTitle] = useState('');
@@ -25,30 +26,32 @@ const NewBookForm = () => {
   };
 
   return (
-    <div>
-      <h2>Add New Book</h2>
-      <form>
+    <div className="form-container">
+      <h2 className="form-title">Add New Book</h2>
+      <form className="form">
         <input
+          className="title-input"
           type="text"
           placeholder="Book title"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
         <input
+          className="author-input"
           type="text"
           placeholder="Author"
           value={author}
           onChange={(e) => setAuthor(e.target.value)}
         />
-        <select value={category} onChange={(e) => setCategory(e.target.value)}>
-          <option value="" disabled selected>Select an option</option>
+        <select className="category-select" value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="" disabled>Select an option</option>
           <option value="Fiction">Fiction</option>
           <option value="Mystery/Thriller">Mystery/Thriller</option>
           <option value="Fantasy">Fantasy</option>
           <option value="Science and Nature">Science and Nature</option>
           <option value="Poetry">Poetry</option>
         </select>
-        <button type="button" className="btn" onClick={handleAddBook}>Add Book</button>
+        <button type="button" className="add-btn" onClick={handleAddBook}>Add Book</button>
       </form>
     </div>
   );
